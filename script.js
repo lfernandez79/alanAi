04eff871cbaaeccb1947de1c2f265c7f5e879d89
LeoFdz <lfernandez79@gmail.com>
2021-06-23 11:36:03 -0500

    initial setup for AlanAi challenge, basic html, js file are connected, also contains a test rendering to DOm from JS

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let tree = document.getElementById("BST");
+
+const rendertest = () => {
+    tree.innerHTML = "Just a quick test to make sure there is a rendering from JS to HTML into DOM"
+}
+
+rendertest();
+
