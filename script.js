c81e8f798604e3c69f1e3755f4167433855b32ea
LeoFdz <lfernandez79@gmail.com>
2021-06-23 13:41:07 -0500

    started to add basic layout of the tree, left and right node, set value to null. also, another class for root with value of null

--- script.js
+++ script.js
@@ -2,8 +2,23 @@
 let tree = document.getElementById("BST");
 
 const rendertest = () => {
-    tree.innerHTML = "Just a quick test to make sure there is a rendering from JS to HTML into DOM"
+    tree.innerHTML = "Just a quick test to make sure there is a rendering from JS to HTML into DOM, this could change as i see it developing"
 }
-
 rendertest();
 
+
+// BST
+
+class TreeNode {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class RootSearchTree {
+    constructor() {
+        this.root = null;
+    }
+}
